453d7e182718330bb2283726ce34cf6215aaf98d
Ophélie <ophelie.delsaux@insa-lyon.fr>
2014-12-12 17:33:06 +0100

    Création de l'interface Serveur

--- src/stream/InterfaceServeur.java
+++ src/stream/InterfaceServeur.java
@@ -4,12 +4,18 @@ package stream;
  * Created by Ophélie on 12/12/2014.
  */
 
-// Pour créer des fenêtres
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.border.Border;
+import java.awt.BorderLayout;
 
 public class InterfaceServeur extends JFrame {
+
+    private JFrame fenetre = new JFrame();
+    private JPanel panelPrincipal = new JPanel();
+    private JButton boutonServeur = new JButton();
 
     public InterfaceServeur () {
         /**
@@ -19,9 +25,7 @@ public class InterfaceServeur extends JFrame {
         // Définition des variables
         String titreFenetre = new String("Serveur");
 
-        // Création de la fenêtre
-        JFrame fenetre = new JFrame();
-
+        // CREATION DE LA FENETRE
         // Ajouter un titre à la fenêtre
         fenetre.setTitle(titreFenetre);
         // Dimensionner la fenêtre
@@ -31,16 +35,22 @@ public class InterfaceServeur extends JFrame {
         // Termine le processus quand on quitte la fenêtre
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Création d'un JPanel dont on va se servir pour ajouter des boutons...etc...
-        JPanel panelPrincipal = new JPanel();
-
+        //CREATION DU JPANEL dont on va se servir pour ajouter des boutons...etc...
         // Définition de sa couleur
         panelPrincipal.setBackground(Color.black);
 
         // La JFrame doit être liée à ce JPanel !
         fenetre.setContentPane(panelPrincipal);
 
-        // Rendre visible la fenêtre
+        // CREATION DES BOUTONS
+        // Création du boutton pour mettre en ligne ou non le serveur
+        boutonServeur.setText("Lancement");
+        // Définition du layout à utiliser
+        fenetre.setLayout(new BorderLayout());
+        // Placement du bouton sur le panel principal
+        fenetre.getContentPane().add(boutonServeur, BorderLayout.SOUTH);
+
+        // RENDRE VISIBLE LA FENETRE
         fenetre.setVisible(true);
     }
 }
