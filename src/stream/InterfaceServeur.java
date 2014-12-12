af3d5f060812062980377267707e50a3b2731e86
Ophélie <ophelie.delsaux@insa-lyon.fr>
2014-12-12 18:05:05 +0100

    Création de l'interface Serveur

--- src/stream/InterfaceServeur.java
+++ src/stream/InterfaceServeur.java
@@ -6,15 +6,17 @@ package stream;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.Color;
+import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.border.Border;
-import java.awt.BorderLayout;
 
 public class InterfaceServeur extends JFrame {
 
     private JFrame fenetre = new JFrame();
     private JPanel panelPrincipal = new JPanel();
+    private JPanel sousPanelTexte = new JPanel();
+    private JPanel sousPanelChamp = new JPanel();
+    private JPanel sousPanelBouton = new JPanel();
     private JButton boutonServeur = new JButton();
 
     public InterfaceServeur () {
@@ -35,20 +37,37 @@ public class InterfaceServeur extends JFrame {
         // Termine le processus quand on quitte la fenêtre
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //CREATION DU JPANEL dont on va se servir pour ajouter des boutons...etc...
-        // Définition de sa couleur
-        panelPrincipal.setBackground(Color.black);
-
-        // La JFrame doit être liée à ce JPanel !
-        fenetre.setContentPane(panelPrincipal);
+        // MODIFICATION DES JPANEL dont on va se servir pour ajouter des boutons...etc...
+        // Définition du panel principal
+        panelPrincipal.setLayout(new GridLayout(3, 1));
+        // Définition de la couleur des trois panels
+        sousPanelTexte.setBackground(Color.black);
+        sousPanelChamp.setBackground(Color.black);
+        sousPanelBouton.setBackground(Color.black);
+        // Définition du panel du bouton
+        sousPanelBouton.setLayout(new FlowLayout());
 
         // CREATION DES BOUTONS
         // Création du boutton pour mettre en ligne ou non le serveur
         boutonServeur.setText("Lancement");
-        // Définition du layout à utiliser
-        fenetre.setLayout(new BorderLayout());
-        // Placement du bouton sur le panel principal
-        fenetre.getContentPane().add(boutonServeur, BorderLayout.SOUTH);
+        boutonServeur.setPreferredSize(new Dimension(100, 50));
+        // Dimensionner le bouton
+
+        // PLACEMENT DES DIFFERENTS JPANEL
+        // Le sousPanelTexte prend le texte
+
+        // Le sousPanelChamp prend le champ
+
+        // Le sousPanelBouton prend le bouton
+        sousPanelBouton.add(boutonServeur);
+
+        // Le panelPrincipal prend les trois JPanel
+        panelPrincipal.add(sousPanelTexte);
+        panelPrincipal.add(sousPanelChamp);
+        panelPrincipal.add(sousPanelBouton);
+
+        // La JFrame doit être liée à ce JPanel !
+        fenetre.setContentPane(panelPrincipal);
 
         // RENDRE VISIBLE LA FENETRE
         fenetre.setVisible(true);
